88422792b988cd8c92d4da24ab08450484444e9b
Daniel Usov <dan.usov82@gmail.com>
2022-12-17 12:18:31 +0300

    add new styles

--- src/FrontEnd/components/tasksList/tasksList.js
+++ src/FrontEnd/components/tasksList/tasksList.js
@@ -17,16 +17,22 @@ export class TasksList {
 		customСheckboxInput.setAttribute('id', `checkbox-input-${id}`);
 		customСheckboxInput.classList.add('custom-checkbox-input');
 
-		if (is_done) {
-			customСheckboxInput.checked = true;
-		} else {
-			customСheckboxInput.checked = false;
-		}
-
 		const taskHeader = document.createElement('label');
 		taskHeader.textContent = header;
 		taskHeader.setAttribute('for', `checkbox-input-${id}`);
 		taskHeader.classList.add('task-header');
+
+		if (is_done) {
+			customСheckboxInput.checked = true;
+			taskField.classList.add('checked');
+			customСheckboxInput.classList.add('checked');
+			taskHeader.classList.add('checked');
+		} else {
+			customСheckboxInput.checked = false;
+			taskField.classList.remove('checked');
+			customСheckboxInput.classList.remove('checked');
+			taskHeader.classList.remove('checked');
+		}
 
 		taskField.append(customСheckboxInput, taskHeader);
 		tasksContainer.append(taskField);
